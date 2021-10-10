4fc896ab463180e8c83f39c7e08ece582ee81cec
Art Kelly <artchkelly12@gmail.com>
2021-10-10 10:23:19 +0100

    Day2 part2

--- Challenges/Day2/solve.go
+++ Challenges/Day2/solve.go
@@ -12,18 +12,11 @@ func main() {
 	lines, err := util.ReadLines("Challenges/Day2/input.txt")
 	util.Check(err)
 
-	var currentPassword []string
-	var count int = 0
+	count := partOne(lines)
+	fmt.Printf("Part 1: %d\n", count)
 
-	for i := 0; i < len(lines); i++ {
-		currentPassword = strings.Fields(lines[i])
-		rule, character, password := currentPassword[0], strings.TrimSuffix(currentPassword[1], ":"), currentPassword[2]
-		lowerBound, upperBound := getBounds(rule)
-		if lowerBound <= strings.Count(password, character) && strings.Count(password, character) <= upperBound {
-			count++
-		}
-	}
-	fmt.Printf("Part 1: %d", count)
+	count = partTwo(lines)
+	fmt.Printf("Part 2: %d\n", count)
 }
 
 func getBounds(str string) (int, int) {
@@ -36,4 +29,36 @@ func getBounds(str string) (int, int) {
 	util.Check(err)
 
 	return lowerBound, upperBound
+}
+
+func partOne(lines []string) int {
+	var currentPassword []string
+	var count int = 0
+
+	for i := 0; i < len(lines); i++ {
+		currentPassword = strings.Fields(lines[i])
+		rule, character, password := currentPassword[0], strings.TrimSuffix(currentPassword[1], ":"), currentPassword[2]
+		lowerBound, upperBound := getBounds(rule)
+		if lowerBound <= strings.Count(password, character) && strings.Count(password, character) <= upperBound {
+			count++
+		}
+	}
+	return count
+}
+
+func partTwo(lines []string) int {
+	var currentPassword []string
+	var count int = 0
+	var a, b bool = false, false
+
+	for i := 0; i < len(lines); i++ {
+		currentPassword = strings.Fields(lines[i])
+		rule, character, password := currentPassword[0], strings.TrimSuffix(currentPassword[1], ":"), currentPassword[2]
+		lowerBound, upperBound := getBounds(rule)
+		a, b = string(password[lowerBound-1]) == character, string(password[upperBound-1]) == character
+		if a != b {
+			count++
+		}
+	}
+	return count
 }
