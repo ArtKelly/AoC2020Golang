46a56406276ec4ecf431fce3458f4c9f09fa050e
Art Kelly <artchkelly12@gmail.com>
2021-10-10 11:16:00 +0100

    Day3 Part2

--- Challenges/Day3/solve.go
+++ Challenges/Day3/solve.go
@@ -9,11 +9,20 @@ import (
 func main() {
 	lines, err := util.ReadLines("Challenges/Day3/input.txt")
 	util.Check(err)
+
+	trees := countTrees(lines, 3, 1)
+	fmt.Printf("Part 1 trees: %d\n", trees)
+
+	part2 := 1 * countTrees(lines, 1, 1) * countTrees(lines, 3, 1) * countTrees(lines, 5, 1) * countTrees(lines, 7, 1) * countTrees(lines, 1, 2)
+	fmt.Printf("Part 2 trees: %d\n", part2)
+}
+
+func countTrees(lines []string, dx int, dy int) int {
 	var trees int = 0
-	for x, y := 0, 0; y < len(lines); x, y = x+3, y+1 {
+	for x, y := 0, 0; y < len(lines); x, y = x+dx, y+dy {
 		if string(lines[y][x%len(lines[y])]) == "#" {
 			trees++
 		}
 	}
-	fmt.Printf("Trees: %d\n", trees)
+	return trees
 }
